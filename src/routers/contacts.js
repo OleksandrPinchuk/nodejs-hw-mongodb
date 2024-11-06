6d7c0cf6312007785b756cb22922ecc70fbe7cf3
OleksandrPinchuk <hohhua@gmail.com>
2024-11-06 20:36:07 +0100

    routers

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { getContactsController, getContactsByIdController, createContactController, upsertContactController, deleteContactController, patchContactController } from "../controllers/contacts.js";
+import {
+    getContactsController,
+    getContactsByIdController,
+    createContactController,
+    upsertContactController,
+    deleteContactController,
+    patchContactController
+} from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
