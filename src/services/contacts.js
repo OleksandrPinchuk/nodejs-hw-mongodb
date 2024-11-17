920194d86dff2fe5e53b5489235aee957816d526
OleksandrPinchuk <116524115+OleksandrPinchuk@users.noreply.github.com>
2024-11-17 22:44:27 +0100

    clear comments

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -5,8 +5,6 @@ import { calculatePaginationData } from "../utils/calculatePaginationData.js";
 export const getContacts = async ({ page = 1, perPage = 10, sortOrder = SORT_ORDER.ASC, sortBy = "_id", filter = {}, }) => {
     const limit = perPage;
     const skip = (page - 1) * perPage;
-    // const contactsQuery = ContactsCollection.find();
-    // const contactsCount = await ContactsCollection.find().merge(contactsQuery).countDocuments();
     const contactsQuery = ContactsCollection.find();
     if (filter.contactType) {
         contactsQuery.where("contactType").equals(filter.contactType);
@@ -16,7 +14,6 @@ export const getContacts = async ({ page = 1, perPage = 10, sortOrder = SORT_ORD
     };
 
     const contactsCount = await ContactsCollection.find().merge(contactsQuery).countDocuments();
-        
     const contacts = await contactsQuery.skip(skip).limit(limit).sort({[sortBy]: sortOrder}).exec();
     const paginationData = calculatePaginationData(contactsCount, perPage, page);
 
